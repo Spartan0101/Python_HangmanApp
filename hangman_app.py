1e0d441e4401d6591b00417f1a3dbedf40199c5b
Spartan1010 <tranulovianos@gmail.com>
2024-05-23 13:03:47 +0100

    Hangman App

--- hangman_app.py
+++ hangman_app.py
@@ -1,1 +1,47 @@
-print("Hangman app")
+import random
+from replit import clear
+import art
+import words
+
+print(art.logo)
+
+chosen_word = random.choice(words.word_list)
+word_length = len(chosen_word)
+end_of_game = False
+lives = 6
+
+#print(chosen_word)
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+
+while not end_of_game:
+    guess_letter = input("Guess a letter: ").lower()
+
+    clear()
+
+    if guess_letter in display:
+        print(f"You've already guessed {guess_letter}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess_letter}")
+        if letter == guess_letter:
+            display[position] = letter
+
+    if guess_letter not in chosen_word:
+        print(f"{guess_letter} is not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win")
+
+    print(art.stages[lives])
